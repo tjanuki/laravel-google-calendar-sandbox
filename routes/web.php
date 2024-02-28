185c00c1c1ff7551285213bc479391a041dcf48c
tjanuki <t.januki@gmail.com>
2024-02-27 19:46:00 -0500

    Install filament panel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Handle the Filament routes.
+//Route::get('/', function () {
+//    return view('welcome');
+//});
