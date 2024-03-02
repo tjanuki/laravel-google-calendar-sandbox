bc65a9f2006c8a6a4643308bc2ea751814c43a36
tjanuki <t.januki@gmail.com>
2024-03-02 15:20:24 -0500

    Create Google Calendar and delete it

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\OAuthToken;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -53,4 +54,86 @@ Route::get('/calendars', function () {
     $calendarList = $service->calendarList->listCalendarList();
 
     dd($calendarList->getItems());
+});
+
+Route::get('/calendars/create', function () {
+    $user = auth()->user();
+
+    $client = new \Google_Client();
+    $client->setAccessToken($user->oauthToken->token);
+    $service = new \Google_Service_Calendar($client);
+
+    $startDateTime = Carbon::create(2024, 3, 3, 9, 0, 0, 'Asia/Tokyo');
+    $endDateTime = Carbon::create(2024, 3, 3, 17, 0, 0, 'Asia/Tokyo');
+
+    $event = new Google_Service_Calendar_Event(array(
+        'summary' => 'Google I/O 2025',
+        'location' => '800 Howard St., San Francisco, CA 94103',
+        'description' => 'A chance to hear more about Google\'s developer products.',
+        'start' => array(
+            'dateTime' => $startDateTime->toRfc3339String(),
+            'timeZone' => $startDateTime->getTimezone()->getName(),
+        ),
+        'end' => array(
+            'dateTime' => $endDateTime->toRfc3339String(),
+            'timeZone' => $endDateTime->getTimezone()->getName(),
+        ),
+        'reminders' => array(
+            'useDefault' => FALSE,
+            'overrides' => array(
+                array('method' => 'popup', 'minutes' => 10),
+            ),
+        ),
+    ));
+
+    $calendarId = 'primary';
+    $event = $service->events->insert($calendarId, $event);
+
+    dd($event);
+});
+
+Route::get('/calendars/find', function () {
+    $user = auth()->user();
+
+    $client = new \Google_Client();
+    $client->setAccessToken($user->oauthToken->token);
+    $service = new \Google_Service_Calendar($client);
+
+    $startDateTime = Carbon::create(2024, 3, 3, 9, 0, 0, 'Asia/Tokyo');
+    $endDateTime = Carbon::create(2024, 3, 3, 17, 0, 0, 'Asia/Tokyo');
+
+    $calendarId = 'primary';
+    $optParams = array(
+        'timeMin' => $startDateTime->toRfc3339String(), // Start of your date/time range in RFC3339 format
+        'timeMax' => $endDateTime->toRfc3339String(), // End of your date/time range in RFC3339 format
+        'singleEvents' => true,
+        'orderBy' => 'startTime',
+    );
+    $events = $service->events->listEvents($calendarId, $optParams);
+
+    $matchedEventId = null;
+
+    foreach ($events->getItems() as $event) {
+        $eventStart = $event->start->dateTime;
+        $eventEnd = $event->end->dateTime;
+
+        $eventStartTokyo = Carbon::parse($eventStart)->timezone('Asia/Tokyo')->toRfc3339String();
+        $eventEndTokyo = Carbon::parse($eventEnd)->timezone('Asia/Tokyo')->toRfc3339String();
+
+        if ($eventStartTokyo == $startDateTime->toRfc3339String()
+            && $eventEndTokyo == $endDateTime->toRfc3339String()
+        ) {
+            $matchedEventId = $event->getId();
+            break; // Exit the loop once a matching event is found
+        }
+    }
+
+    if ($matchedEventId) {
+        $service->events->delete($calendarId, $matchedEventId);
+        return response()->json(['success' => true, 'message' => 'Event deleted successfully.']);
+
+    } else {
+        // No matching event found
+        return "No event found with the specified start and end times.";
+    }
 });
